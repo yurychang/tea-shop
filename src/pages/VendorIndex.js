fcf4ed3ff1b4ddca6a04e3fb868284ea3764c1d0
lioushuyu <a8532582@gmail.com>
2020-03-11 11:00:26 +0800

    add VendorIndex

---
+++ src/pages/VendorIndex.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainBar from '../components/MainBar'
+
+
+function VendorIndex (){
+
+
+    return (
+        <>
+            <MainBar/>
+            
+        </>
+    )
+}
+
+export default VendorIndex
