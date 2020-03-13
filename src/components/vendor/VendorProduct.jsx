90844568b32538f9764e819e370e8e283e4f5f6d
Liou <a8532582@gmail.com>
2020-03-13 10:24:32 +0800

    addVendorProduct

---
+++ src/components/vendor/VendorProduct.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+
+
+function VendorAbout() {
+    return (
+        <>
+
+            <div className="product d-flex justify-content flex-wrap flex-grow-1 ml-5">
+
+                <div className="card
+singleProduct m-3">
+                    <img className="" src="https://via.placeholder.com/250" alt="" />
+                    <div className="card-body">
+                        <h5 className="card-title">凍頂烏龍茶</h5>
+                        <div className="card-text">NTD 200</div>
+                    </div>
+                </div>
+
+
+                <div className="card singleProduct">
+                    <img className="" src="https://via.placeholder.com/250" alt="" />
+                    <div className="card-body">
+                        <h5 className="card-title">凍頂烏龍茶</h5>
+                        <div className="card-text">NTD 200</div>
+                    </div>
+                </div>
+
+                <div className="card m-3 singleProduct">
+                    <img className="" src="https://via.placeholder.com/250" alt="" />
+                    <div className="card-body">
+                        <h5 className="card-title">凍頂烏龍茶</h5>
+                        <div className="card-text">NTD 200</div>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </>
+    )
+}
+
+export default VendorAbout
