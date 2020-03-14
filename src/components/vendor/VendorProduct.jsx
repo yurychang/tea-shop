6e0191ea90aa5a327665af606c41dc0a424efbc3
Liou <a8532582@gmail.com>
2020-03-14 20:37:46 +0800

    addVendorRouterUpdateVendorProduct

--- src/components/vendor/VendorProduct.jsx
+++ src/components/vendor/VendorProduct.jsx
@@ -11,8 +11,20 @@ function VendorProduct() {
                 <div className="card singleProduct mr-3">
                     <img className="" src="https://via.placeholder.com/250" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">凍頂烏龍茶</h5>
-                        <div className="card-text">NTD 200</div>
+                        <div className="producttop mb-2 d-flex justify-content-between">
+                            <div className="ptag">
+                                <button type="button" class="btn btn-outline-primary btn-xs mr-1">高海拔</button>
+                                <button type="button" class="btn btn-outline-info btn-xs mr-1">無農藥</button>
+                                <button type="button" class="btn btn-outline-danger btn-xs">蜜香紅茶</button>
+                            </div>
+                            <i class="far fa-heart mt-2"></i>
+                        </div>
+                        <h6 className="">凍頂烏龍茶</h6>
+                        <div className="product-pretext mb-2">風華絕代50年，山蔭長歌採茶人......</div>
+                        <div className="productbottom d-flex justify-content-between">
+                            <div className="product-price">NTD 200</div>
+                            <i className="fas fa-shopping-cart mt-1" />
+                        </div>
                     </div>
                 </div>
 
@@ -20,24 +32,62 @@ function VendorProduct() {
                 <div className="card singleProduct mr-3">
                     <img className="" src="https://via.placeholder.com/250" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">凍頂烏龍茶</h5>
-                        <div className="card-text">NTD 200</div>
+                        <div className="producttop mb-2 d-flex justify-content-between">
+                            <div className="ptag">
+                                <button type="button" class="btn btn-outline-primary btn-xs mr-1">高海拔</button>
+                                <button type="button" class="btn btn-outline-info btn-xs mr-1">無農藥</button>
+                                <button type="button" class="btn btn-outline-danger btn-xs">蜜香紅茶</button>
+                            </div>
+                            <i class="far fa-heart mt-2"></i>
+                        </div>
+                        <h6 className="">凍頂烏龍茶</h6>
+                        <div className="product-pretext mb-2">風華絕代50年，山蔭長歌採茶人......</div>
+                        <div className="productbottom d-flex justify-content-between">
+                            <div className="product-price">NTD 200</div>
+                            <i className="fas fa-shopping-cart mt-1" />
+                        </div>
                     </div>
                 </div>
+
 
                 <div className="card singleProduct mr-3">
                     <img className="" src="https://via.placeholder.com/250" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">凍頂烏龍茶</h5>
-                        <div className="card-text">NTD 200</div>
+                        <div className="producttop mb-2 d-flex justify-content-between">
+                            <div className="ptag">
+                                <button type="button" class="btn btn-outline-primary btn-xs mr-1">高海拔</button>
+                                <button type="button" class="btn btn-outline-info btn-xs mr-1">無農藥</button>
+                                <button type="button" class="btn btn-outline-danger btn-xs">蜜香紅茶</button>
+                            </div>
+                            <i class="far fa-heart mt-2"></i>
+                        </div>
+                        <h6 className="">凍頂烏龍茶</h6>
+                        <div className="product-pretext mb-2">風華絕代50年，山蔭長歌採茶人......</div>
+                        <div className="productbottom d-flex justify-content-between">
+                            <div className="product-price">NTD 200</div>
+                            <i className="fas fa-shopping-cart mt-1" />
+                        </div>
                     </div>
                 </div>
+
 
                 <div className="card singleProduct mr-3">
                     <img className="" src="https://via.placeholder.com/250" alt="" />
                     <div className="card-body">
-                        <h5 className="card-title">凍頂烏龍茶</h5>
-                        <div className="card-text">NTD 200</div>
+                        <div className="producttop mb-2 d-flex justify-content-between">
+                            <div className="ptag">
+                                <button type="button" class="btn btn-outline-primary btn-xs mr-1">高海拔</button>
+                                <button type="button" class="btn btn-outline-info btn-xs mr-1">無農藥</button>
+                                <button type="button" class="btn btn-outline-danger btn-xs">蜜香紅茶</button>
+                            </div>
+                            <i class="far fa-heart mt-2"></i>
+                        </div>
+                        <h6 className="">凍頂烏龍茶</h6>
+                        <div className="product-pretext mb-2">風華絕代50年，山蔭長歌採茶人......</div>
+                        <div className="productbottom d-flex justify-content-between">
+                            <div className="product-price">NTD 200</div>
+                            <i className="fas fa-shopping-cart mt-1" />
+                        </div>
                     </div>
                 </div>
 
--- src/components/vendor/VendorRouter.jsx
+++ src/components/vendor/VendorRouter.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import VendorAbout from '.'
+import VendorAbout from '../../pages/VendorAbout'
+import VendorIndex from '../../pages/VendorIndex'
 
 function VendorRouter() {
 
     return (
-        <Route path='/vendorabout/:id?'>
-
-
+        <>
+        <Route path='/vendorindex/:id?'>
+            <VendorIndex />
         </Route>
+        <Route path='/vendorabout/:id?'>
+            <VendorAbout />
+        </Route>
+        </>
     )
 
 }
 
+export default VendorRouter
