48b7f77e81be25430040ab4602c965c0b50a0be4
Luke <101416018@stud.sju.edu>
2020-03-24 12:03:23 +0800

    23231

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { NavLink, Link } from 'react-router-dom'
 
-
 export default function Login() {
   return (
     <div className="container yz-header mb-2">
       <ul className="d-flex justify-content-between">
-        <li className="nav-item mt-3"><NavLink className="nav-link pb-2 ls_login-nav" activeClassName="active" to="/login">會員登入</NavLink></li>
-        <li className="nav-item mt-3"><NavLink className="nav-link pb-2 ls_login-nav mr-5" activeClassName="active" exact to="/login/vendor">廠商登入</NavLink></li>
+        <li className="nav-item mt-3">
+          <NavLink
+            className="nav-link pb-2 ls_login-nav"
+            activeClassName="active"
+            to="/login"
+          >
+            會員登入
+          </NavLink>
+        </li>
+        <li className="nav-item mt-3">
+          <NavLink
+            className="nav-link pb-2 ls_login-nav mr-5"
+            activeClassName="active"
+            exact
+            to="/login/vendor"
+          >
+            廠商登入
+          </NavLink>
+        </li>
       </ul>
 
       <form>
@@ -28,15 +44,12 @@ export default function Login() {
           />
         </div>
         <div className="form-group form-check d-flex">
-          <input
-            type="checkbox"
-            className="form-check-input"
-          />
+          <input type="checkbox" className="form-check-input" />
           <div>
             <div>
               <label className="form-check-label" for="exampleCheck1">
                 記住帳號密碼
-                </label>
+              </label>
               <div>
                 <div className="yz-froget">
                   <Link to="#">忘記密碼？</Link>
@@ -49,14 +62,17 @@ export default function Login() {
           </div>
           <Link type="button" className="yz_btn-login text-center">
             <i className="fas fa-sign-in-alt"> </i>
-            登入</Link>
+            登入
+          </Link>
         </div>
         <Link type="button" className="fb text-center">
           <i className="fab fa-facebook-f"></i>
-          Facebook</Link>
+          Facebook
+        </Link>
         <Link type="button" className="gmail text-center">
           <i className="far fa-envelope"> </i>
-          Gmail</Link>
+          Gmail
+        </Link>
       </form>
     </div>
   )
